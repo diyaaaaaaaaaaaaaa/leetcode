65dfc70b08fab6bf2df017a8a5efcd15b8fd72e7
diyaaaaaaaaaaaaaa <prachichoudhary0704@gmail.com>
2026-08-24 12:25:45 +0530

    BS

---
+++ binary_search/bs.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int high=nums.size()-1, low=0;
+
+        while(high>=low){
+            int mid=(high-low)/2 + low;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+
+        return -1;
+    }
+};
+
