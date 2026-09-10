333951e9d87266aacb60bb3fea034466f33434cb
diyaaaaaaaaaaaaaa <prachichoudhary0704@gmail.com>
2026-09-10 18:12:15 +0530

    annoyingest recursion qn (yet)

---
+++ recursion/78.subsets/78.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void f(int id, int n, vector<int>& smol, vector<int>& nums, vector<vector<int>>& ans){
+        if (id>=n) {
+            ans.push_back(smol);
+            return;
+        }
+
+        smol.push_back(nums[id]);
+        f(id+1, n, smol, nums, ans);
+
+        smol.pop_back();
+        f(id+1, n, smol, nums, ans);
+    }
+   
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n=nums.size();
+        vector<int> smol;
+        f(0, n, smol, nums, ans);
+
+        return ans;
+    }
+};
